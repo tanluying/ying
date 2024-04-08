8f293ea063f5b241d13c30dd92b3143b2cfe1642
幻tly <81273793+tanluying@users.noreply.github.com>
2024-04-08 14:31:38 +0800

    Update pcjb.bat

--- pcjb.bat
+++ pcjb.bat
@@ -8,3 +8,8 @@ ping -n 2 127.0.0.1>nul
 WScript.CreateObject("WScript.Shell").SendKeys "start https://b23.tv/6ycsvfV"
 ping -n 2 127.0.0.1>nul
 WScript.CreateObject("WScript.Shell").SendKeys "{ENTER}"
+
+@echo off
+echo MsgBox "你的电脑已经被入侵", vbOKOnly, "对话框标题" > %temp%\tempmessage.vbs
+cscript //nologo %temp%\tempmessage.vbs
+del %temp%\tempmessage.vbs
