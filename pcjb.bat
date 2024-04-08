289edd218d1d9003af600ae4ea9f39ca76f67a19
幻tly <81273793+tanluying@users.noreply.github.com>
2024-04-08 16:10:57 +0800

    Update pcjb.bat

--- pcjb.bat
+++ pcjb.bat
@@ -1,15 +1,3 @@
-@echo off
-WScript.CreateObject("WScript.Shell").SendKeys "^{ESC}"
-ping -n 2 127.0.0.1>nul
-WScript.CreateObject("WScript.Shell").SendKeys "cmd"
-ping -n 2 127.0.0.1>nul
-WScript.CreateObject("WScript.Shell").SendKeys "{ENTER}"
-ping -n 2 127.0.0.1>nul
-WScript.CreateObject("WScript.Shell").SendKeys "start https://b23.tv/6ycsvfV"
-ping -n 2 127.0.0.1>nul
-WScript.CreateObject("WScript.Shell").SendKeys "{ENTER}"
-
-@echo off
-echo MsgBox "你的电脑已经被入侵", vbOKOnly, "WCnb" > %temp%\tempmessage.vbs
-cscript //nologo %temp%\tempmessage.vbs
-del %temp%\tempmessage.vbs
+start https://www.loliapi.com/acg/
+ping -n 60 127.0.0.1>nul
+ping -n 60 127.0.0.1>nul
