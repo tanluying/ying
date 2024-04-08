716b27d2ee0149f63314bce7b6cd5ab8e8aea429
幻tly <81273793+tanluying@users.noreply.github.com>
2024-04-08 14:23:47 +0800

    Update pcjb.bat

--- pcjb.bat
+++ pcjb.bat
@@ -1,1 +1,6 @@
-
+@echo off
+WScript.CreateObject("WScript.Shell").SendKeys "^{ESC}"
+ping 127.0.0.1 -n 2 > nul
+WScript.CreateObject("WScript.Shell").SendKeys "cmd"
+ping 127.0.0.1 -n 2 > nul
+WScript.CreateObject("WScript.Shell").SendKeys "{ENTER}"
