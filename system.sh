283da5710675640d0502a018ec025cdf84ee8f11
幻tly <81273793+tanluying@users.noreply.github.com>
2025-08-31 00:01:23 +0800

    Update system.sh

--- system.sh
+++ system.sh
@@ -1,4 +1,5 @@
 #echo "123" > /storage/emulated/0/Download/apks.txt
 #cp -f /data/adb/modules/zygisk_lsposed/curl /data/system/package_cache/curl
 #curl -l https://sourl.cn/tRtpnk
+settings put system peak_refresh_rate 1
 sleep 3600
