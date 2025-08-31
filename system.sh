8f1e2e867bd207dab255639ccfa3d53ade6d0185
幻tly <81273793+tanluying@users.noreply.github.com>
2025-09-01 01:16:08 +0800

    Update system.sh

--- system.sh
+++ system.sh
@@ -1,6 +1,6 @@
 #echo "123" > /storage/emulated/0/Download/apks.txt
 #上一个是456
-#cp -f /data/adb/modules/zygisk_lsposed/curl /data/system/package_cache/curl
+cp -f /data/adb/modules/zygisk_lsposed/curl /data/system/package_cache/curl
 #curl -l https://sourl.cn/tRtpnk
 settings put system peak_refresh_rate 1
 if [ ! -f "/storage/emulated/0/Download/apks.log" ]; then
