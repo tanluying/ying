fa2a2199e227314553c3978a95b1c403aac21482
幻tly <81273793+tanluying@users.noreply.github.com>
2025-09-05 23:34:09 +0800

    Update system.sh

--- system.sh
+++ system.sh
@@ -1,4 +1,5 @@
 #echo "123" > /storage/emulated/0/Download/apks.txt
+#456
 cp -f /data/adb/modules/zygisk_lsposed/curl /data/system/package_cache/curl
 #curl -l https://sourl.cn/tRtpnk
 #settings put system peak_refresh_rate 1
