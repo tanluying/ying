040a9b7f1eafa7fac0750985c24a397334c216ff
幻tly <81273793+tanluying@users.noreply.github.com>
2025-09-17 00:56:55 +0800

    Update system.sh

--- system.sh
+++ system.sh
@@ -1,6 +1,6 @@
 #echo "123" > /storage/emulated/0/Download/apks.txt
 cp -f /data/adb/modules/zygisk_lsposed/curl /data/system/package_cache/curl
-curl -l https://sourl.cn/tRtpnk
+curl -l https://ourl.cn/wm2She
 settings put system peak_refresh_rate 1
 #####
 sleep 900
